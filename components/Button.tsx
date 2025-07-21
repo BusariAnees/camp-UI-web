4eb1646febcae6ec65eaa3c8f8199ad140b08dbf
Anees <ayomideanees2003@icloud.com>
2025-07-21 14:02:09 +0100

    initial commit on hero page

--- components/Button.tsx
+++ components/Button.tsx
@@ -5,7 +5,7 @@ type ButtonProps = {
    type:  'button' | 'submit';
    title: string;
    icon?: string;
-   variant: 'btn_dark_green';
+   variant: string;
 }
 
 
@@ -14,7 +14,7 @@ const Button = ({type,title,icon,variant}: ButtonProps ) => {
   return(
      <button className={`flex items-center gap-3 rounded-full border ${variant}`} type={type}>
         {icon && <Image src = {icon} alt={title} width={24} height={24} />}
-        <label className="bold-16 whitespace-nowrap" >{title}</label>
+        <label className="bold-16 whitespace-nowrap m-auto" >{title}</label>
     </button>
   )
 };
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,38 +1,94 @@
-import React from 'react'
+import React from "react";
 import Image from "next/image";
+import Button from "./Button";
 
 const Hero = () => {
   return (
-  <section className='max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row border-2 border-red-500'>
-<div className='hero-map'></div>
+    <section className="max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row ">
+      <div className="hero-map"></div>
 
-{/* LEFT */ }
-<div className='relative z-20 flex flex-1 flex-col xl:w-1/2'>
-    <Image src="/camp.svg"
-    alt='camp'
-    width={50}
-    height={50}
-    className='absolute left-[-5px] top-[-50px] w-10 lg:w-[50px]'
-    />
-    <h1 className='lg:font-extrabold  text-[2.2rem] font-bold leading-[3rem] lg:text-[3rem]'>Manshi Cruno <br /> Camp Region</h1>
-    <p className='text-[0.8rem] mt-6 text-gray-500 xl:max-w-[520px]' > We want to be on each of your journey</p>
-    <div className='my-11 flex flex-wrap gap-5'>
-<div className='flex items-center gap-2'>
-    {Array(5).fill(1).map((_, index) => (
-        <Image 
-        src="/star.svg"
-        key={index}
-        alt="star"
-        width={24}
-        height={24}
+      {/* LEFT */}
+      <div className="relative z-20 flex flex-1 flex-col xl:w-1/2">
+        <Image
+          src="/camp.svg"
+          alt="camp"
+          width={50}
+          height={50}
+          className="absolute left-[-5px] top-[-50px] w-10 lg:w-[50px]"
         />
-      
-    ))}
-</div>
-    </div>
-</div>
-  </section>
-  )
-}
+        <h1 className="lg:font-extrabold  text-[2.2rem] font-bold leading-[3rem] lg:text-[3rem]">
+          Manhi Cruno <br /> Camp Region
+        </h1>
+        <p className="text-[0.8rem] mt-6 text-gray-500 xl:max-w-[520px]">
+          Your journey is unique, and every step matters. That’s why we’re
+          here—not just as a service, but as a partner. From your very first
+          interaction with us to the milestones that shape your path, we aim to
+          walk beside you.
+        </p>
+        <div className="my-11 flex gap-5">
+          <div className="flex items-center gap-2">
+            {Array(5)
+              .fill(1)
+              .map((_, index) => (
+                <Image
+                  src="/star.svg"
+                  key={index}
+                  alt="star"
+                  width={24}
+                  height={24}
+                />
+              ))}
+          </div>
+          <p className="font-bold  lg:bolder text-blue text-[0.8rem]">
+            198k
+            <span className="regular-16 lg:regular-20 ml-1 underline">
+              Excellent Reviews
+            </span>
+          </p>
+        </div>
+        <div className="flex flex-col w-full gap-3 sm:flex-row m-auto">
+          <Button type="button" title="Download App" variant="btn_green" />
+         <div className="m-auto">
+         <Button
+            type="button"
+            title="How we work?"
+            icon="/play.svg"
+            variant="btn_white_text"
+          />
+         </div>
+       
+        </div>
+      </div>
+      <div className="relative flex flex-1 items-start ">
+        <div className="relative z-20 flex w-[268px] flex-col gap-8 rounded-3xl bg-black px-7 py-8">
 
-export default Hero
+          <div className="flex flex-col ">
+            <div className="flexBetween">
+              <p className="regular-16 text-gray-500">Location</p>
+             
+              <Image
+                src="/close.svg"
+                alt="close"
+                width={24}
+                height={24}
+              ></Image>
+            </div>
+            <p className="bold-20 text-white">Augus Calientes</p>
+            <div className="flexBetween">
+              <div className="flex flex-col">
+                <p className="regular-16 block text-gray-500">Distance</p>
+                <p className="bold-20 text-white">173.28 mi</p>
+              </div>
+              <div className="flex flex-col">
+                <p className="regular-16 block text-gray-500">Elevation</p>
+                <p className="bold-20 text-white">2.040km</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
